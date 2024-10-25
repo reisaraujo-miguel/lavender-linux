bbe6e72448a76177a8652854d6583362e712bceb
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-10-24 22:46:20 -0300

    fix global theming and wallpaper I hope

--- build_files/scripts/set-wallpaper.sh
+++ build_files/scripts/set-wallpaper.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-cp $BUILD_FILES_DIR/wallpaper/cat-wallpaper.jpg /usr/share/wallpapers/cat-wallpaper.jpg
+cp $BUILD_FILES_DIR/wallpaper/Felux /usr/share/wallpapers/
 
-ln -sf /usr/share/wallpapers/cat-wallpaper.jpg /usr/share/backgrounds/default.png
-ln -sf /usr/share/wallpapers/cat-wallpaper.jpg /usr/share/backgrounds/default-dark.png
+ln -sf /usr/share/wallpapers/Felux/contents/images/6000x4000.jpg /usr/share/backgrounds/default.png
+ln -sf /usr/share/wallpapers/Felux/contents/images/6000x4000.jpg /usr/share/backgrounds/default-dark.png
