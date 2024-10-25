052f0b14de33e0ee9b2bbd41fbef4559dbe465a2
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-10-24 22:53:41 -0300

    fix set-wallpaper script

--- build_files/scripts/set-wallpaper.sh
+++ build_files/scripts/set-wallpaper.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-cp $BUILD_FILES_DIR/wallpaper/Felux /usr/share/wallpapers/
+cp -r $BUILD_FILES_DIR/wallpaper/Felux /usr/share/wallpapers/
 
 ln -sf /usr/share/wallpapers/Felux/contents/images/6000x4000.jpg /usr/share/backgrounds/default.png
 ln -sf /usr/share/wallpapers/Felux/contents/images/6000x4000.jpg /usr/share/backgrounds/default-dark.png
