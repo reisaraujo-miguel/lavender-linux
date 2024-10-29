ad5a2da626185aae5f3ab829511941dd10932837
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-10-29 12:32:08 -0300

    fix buildfiles path

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ export RELEASE
 RELEASE="$(rpm -E %fedora)"
 [ -z "$RELEASE" ] && { echo "Failed to determine Fedora release"; exit 1; }
 
-export BUILD_FILES_DIR="$(mktemp -d)"
-trap 'rm -rf "${BUILD_FILES_DIR}"' EXIT
+export BUILD_FILES_DIR="/tmp/build_files"
 
 #--- Branding ---#
 BRANDING_SCRIPT="${BUILD_FILES_DIR}/scripts/branding.sh"
