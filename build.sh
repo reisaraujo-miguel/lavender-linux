9dfd47763144d522ab218ecb0adca8cd6ec50184
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-11-07 21:50:50 -0300

    fix build iso

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ export SYSTEM_FILES_DIR="${BUILD_FILES_DIR}/system_files"
 
 # Helper Functions
 execute_script() {
-    local script="$1"
+	local script="$1"
     local script_path="${SCRIPTS_DIR}/${script}"
 
     if [[ ! -x "$script_path" ]]; then
