dbb13eea3fbb77f1942cbec638bf4957953af1de
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2024-12-05 03:26:37 -0300

    feat: add morewaita icon pack and refactor extra repo script

--- build_files/scripts/set-extra-repos.sh
+++ build_files/scripts/set-extra-repos.sh
@@ -3,7 +3,18 @@
 set -ouex pipefail
 
 # install new repos
-cp "$SYSTEM_FILES_DIR"/etc/yum.repos.d/* /etc/yum.repos.d/
+function install_copr_repo() {
+	local package_maintainer="$1"
+	local package_name="$2"
+	local repo_file="/etc/yum.repos.d/${package_maintainer}-${package_name}-fedora-${RELEASE}.repo"
+	local repo_url="https://copr.fedorainfracloud.org/coprs/${package_maintainer}/${package_name}/repo/fedora-${RELEASE}/${package_maintainer}-${package_name}-fedora-${RELEASE}.repo"
+
+	curl -Lo "$repo_file" "$repo_url"
+}
+
+install_copr_repo "atim" "lazygit"
+install_copr_repo "vitallium" "neovim-default-editor"
+install_copr_repo "dusansimic" "themes"
 
 # enable rpmfusion-nonfree-steam
 steam_repo="/etc/yum.repos.d/rpmfusion-nonfree-steam.repo"
