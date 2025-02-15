924dafb621bc4acb83b1ec07c91497cfdf20ee64
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-02-15 12:05:02 -0300

    feat: add scx and map-count config from bazzite

--- scripts/copy-system-files.sh
+++ scripts/copy-system-files.sh
@@ -16,3 +16,9 @@ rm /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override
 
 # Compile schemas
 glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Download configs from Bazzite
+
+curl -L --create-dirs -o /etc/scx_loader/config.toml https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/etc/scx_loader/config.toml
+
+curl -L --create-dirs -o /usr/lib/sysctl.d/10-map-count.conf https://github.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/lib/sysctl.d/70-gaming.conf
