780c84b7b8a7bc27bf7aa1601138e1aaceb322a5
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-03-13 18:40:32 +0000

    escaping schema

--- scripts/update-system-files.sh
+++ scripts/update-system-files.sh
@@ -17,10 +17,10 @@ replace_line() {
 	local replacement="$3"
 
 	local e_pattern
-	e_pattern=$(echo "$pattern" | sed 's/[\/&]/\\&/g')
+	e_pattern=$(echo "$pattern" | perl -pe 's/([\/&])/\\$1/g')
 
 	local e_replacement
-	e_replacement=$(echo "$replacement" | sed 's/[\/&]/\\&/g')
+	e_replacement=$(echo "$replacement" | perl -pe 's/([\/&])/\\$1/g')
 
 	sed -i "s/^${e_pattern}.*/${e_replacement}/" "$file"
 }
@@ -34,8 +34,8 @@ replace_line "$SCHEMAS_FOLDER/zz0-01-bazzite-desktop-silverblue-dash.gschema.ove
 replace_line "$SCHEMAS_FOLDER/zz0-03-bazzite-desktop-silverblue-extensions.gschema.override" "enabled-extensions" \
 	"enabled-extensions = ['gsconnect@andyholmes.github.io', 'tailscale@joaophi.github.com', 'search-light@icedman.github.com', 'caffeine@patapon.info']"
 
-replace_line "$SCHEMAS_FOLDER/zz0-04-bazzite-desktop-silverblue-theme.gschema.override" '\[org.gnome.desktop.interface\]' \
-	'\[org.gnome.desktop.interface\]\nicon-theme=\"MoreWaita\"'
+replace_line "$SCHEMAS_FOLDER/zz0-04-bazzite-desktop-silverblue-theme.gschema.override" "\[org.gnome.desktop.interface\]" \
+	"\[org.gnome.desktop.interface\]\nicon-theme=\"MoreWaita\""
 
 replace_line "$SCHEMAS_FOLDER/zz0-04-bazzite-desktop-silverblue-theme.gschema.override" "button-layout" "button-layout=\":close\""
 
