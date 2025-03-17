2cf6b9f05bb068cb8274a637008327adfc28497b
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-03-17 18:21:40 +0000

    move additional packages to just if possible

--- scripts/update-system-files.sh
+++ scripts/update-system-files.sh
@@ -29,7 +29,7 @@ replace_line() {
 SCHEMAS_FOLDER=/usr/share/glib-2.0/schemas/
 
 replace_line "$SCHEMAS_FOLDER/zz0-01-bazzite-desktop-silverblue-dash.gschema.override" "favorite-apps" \
-	"favorite-apps = ['org.mozilla.firefox.desktop', 'eu.betterbird.Betterbird.desktop', 'org.gnome.Nautilus.desktop', 'io.bassi.Amberol.desktop', 'org.libreoffice.LibreOffice.writer.desktop', 'org.gnome.Software.desktop', 'com.mitchellh.ghostty.desktop']"
+	"favorite-apps = ['org.mozilla.firefox.desktop', 'eu.betterbird.Betterbird.desktop', 'org.gnome.Nautilus.desktop', 'io.bassi.Amberol.desktop', 'org.libreoffice.LibreOffice.writer.desktop', 'org.gnome.Software.desktop', 'org.gnome.Ptyxis.desktop']"
 
 replace_line "$SCHEMAS_FOLDER/zz0-03-bazzite-desktop-silverblue-extensions.gschema.override" "enabled-extensions" \
 	"enabled-extensions = ['gsconnect@andyholmes.github.io', 'tailscale@joaophi.github.com', 'search-light@icedman.github.com', 'caffeine@patapon.info', 'tilingshell@ferrarodomenico.com']"
@@ -38,8 +38,6 @@ replace_line "$SCHEMAS_FOLDER/zz0-04-bazzite-desktop-silverblue-theme.gschema.ov
 	"\[org.gnome.desktop.interface\]\nicon-theme=\"MoreWaita\""
 
 replace_line "$SCHEMAS_FOLDER/zz0-04-bazzite-desktop-silverblue-theme.gschema.override" "button-layout" "button-layout=\":close\""
-
-replace_line "$SCHEMAS_FOLDER/zz0-00-bazzite-desktop-silverblue-global.gschema.override" "terminal=" "terminal='ghostty'"
 
 # Compile schemas
 glib-compile-schemas "$SCHEMAS_FOLDER"
