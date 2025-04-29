f3156d1d1bae81c7ee35f8b95bd0eeb47d5705b9
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-04-28 21:52:55 -0300

    new try

--- scripts/services.sh
+++ scripts/services.sh
@@ -2,27 +2,27 @@
 
 set -ouex pipefail
 
-systemctl enable sddm
+systemctl enable sddm.service --force
 
-systemctl set-default multi-user.target
+systemctl set-default graphical.target --force
 
 # Enable Bluefin DX services
-systemctl enable bluefin-dx-groups.service
-systemctl enable incus-workaround.service
-systemctl enable libvirt-workaround.service
-systemctl enable swtpm-workaround.service
+systemctl enable bluefin-dx-groups.service --force
+systemctl enable incus-workaround.service --force
+systemctl enable libvirt-workaround.service --force
+systemctl enable swtpm-workaround.service --force
 
 # Enable docker and podman
-systemctl enable docker.socket
-systemctl enable podman.socket
+systemctl enable docker.socket --force
+systemctl enable podman.socket --force
 
 # Enable tailscaled
-systemctl enable tailscaled.service
+systemctl enable tailscaled.service --force
 
 # Enable brew services
-systemctl enable brew-setup.service
-systemctl enable brew-upgrade.timer
-systemctl enable brew-update.timer
+systemctl enable brew-setup.service --force
+systemctl enable brew-upgrade.timer --force
+systemctl enable brew-update.timer --force
 
 # Enable podman services
-systemctl --global enable podman-auto-update.timer
+systemctl --global enable podman-auto-update.timer --force
