744cc867c99bc7e4d6e0239823f0777a36717c71
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2025-04-29 06:44:52 -0300

    remove unused lines

--- scripts/configure-hyprland.sh
+++ scripts/configure-hyprland.sh
@@ -2,13 +2,7 @@
 
 set -ouex pipefail
 
-# Create required directories
-mkdir -p /etc/skel/.config/hypr
-
 chmod +x /usr/share/xdg/autostart/hyprland-portal.desktop
-chmod +x /usr/libexec/bluefin-dx-groups
-chmod +x /usr/libexec/bluefin-incus
-chmod +x /usr/libexec/bluefin-dx-kvmfr-setup
 
 # remove kde plasma
 dnf5 group remove kde-desktop -y
